19b1599a15943652f52d8890c44fff7fbb8133e0
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-09-02 13:13:42 -0300

    aplicando react-hook-form zod @hookform/resolvers

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,28 @@
 import { Search } from "lucide-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+    query: z.string()
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+    const { register, handleSubmit, formState: {isSubmitting} } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    });
+
+    function handleSearchTransactions(data: SearchFormInputs) {
+
+    }
+
     return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Pesquise uma transação" />
-            <button type="submit">
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input type="text" placeholder="Pesquise uma transação" {...register('query')} />
+            <button type="submit" disabled={isSubmitting}>
                 <Search size={15} />
                 Buscar
             </button>
